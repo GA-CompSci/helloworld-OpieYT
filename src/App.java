70bab0eadf420d99acc82033b0c3293bfa2f3bfb
OpieYT <81830938+OpieYT@users.noreply.github.com>
2025-08-26 13:15:41 +0000

    tuesday class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        boolean ryanmoore = true;
+        if (ryanmoore=true) {
+            System.out.println("hello");
+        } 
     }
 }
