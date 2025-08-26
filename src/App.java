593543d7626e60e2e425a746ee862cfd958a9785
OpieYT <81830938+OpieYT@users.noreply.github.com>
2025-08-26 13:16:09 +0000

    more tuesday class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        boolean ryanmoore = true;
-        if (ryanmoore=true) {
-            System.out.println("hello");
+        /*Scanner scanner = new Scanner(System.in);
+        String stuff = scanner.nextLine();
+        System.out.println(stuff); */
+        
+        int x;//decleration
+        x = 5;//initialization
+        char c = 'C';
+        double numbers = 19.5;
+        // camel casing
+        final int CONNY = 5;
+
+        //comparisons
+        boolean boo = false;
+        System.out.println(!boo != false);
+
+        //conditional
+        if(!boo) System.out.println("boo passed");
+
+        //short circuit
+        int divisor = 0;
+        if(divisor !=0 && 5/divisor>1) System.out.println("omg");
+
+        System.out.println(x);
+        System.out.println("Hello, Owen Croniser");
+
+        //scanner.close();
         } 
     }
 }
